605e52d4517b0cbefc4e6664f9133e53e6f66eb7
Ale <alebarbaja@users.noreply.github.com>
2017-08-09 19:10:23 +0200

    Subida inicial.

---
+++ js/eventos.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  
+  $('#container-mini-item').css('display', 'none');
+  
+  $('#lng').click(function() {
+    
+    $('#container-mini-item').slideToggle('fast');
+    
+  })
+  
+})
